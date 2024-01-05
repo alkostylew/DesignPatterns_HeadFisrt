76fb905c47f6768559648ebf619a503b7747763f
Aleksander Kostylew <aleks.kostylew@gmail.com>
2024-01-05 10:06:34 -0500

    Iterator Pattern

---
+++ src/headfirst/iterator/aleksRadioIterator/ChannelIterator.java
@@ -0,0 +1,8 @@
+package headfirst.iterator.aleksRadioIterator;
+
+public interface ChannelIterator {
+
+    public boolean hasNext();
+
+    public Channel next();
+}
---
+++ src/headfirst/iterator/aleksRadioIterator/ChannelTypeEnum.java
@@ -0,0 +1,7 @@
+package headfirst.iterator.aleksRadioIterator;
+
+public enum ChannelTypeEnum {
+
+    ENGLISH, HINDI, FRENCH, ALL;
+}
+
